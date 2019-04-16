eb44d6bbd035e1836e16fff65f12b434a2908db9
Denis Bystruev <dbystruev@me.com>
2019-04-16 07:51:16 +0300

    Initial Commit

---
+++ Time Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Tracker
+//
+//  Created by Denis Bystruev on 16/04/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
